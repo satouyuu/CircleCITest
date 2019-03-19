42865dea0458c551edc62af7ad8a0dd4bfbfd8e2
yu.sato <yu.sato@asia-quest.jp>
2019-03-19 10:23:45 +0900

    Initial Commit

---
+++ CircleCITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleCITest
+//
+//  Created by yu sato on 2019/03/19.
+//  Copyright © 2019 yu sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
